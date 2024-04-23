d96f1d4ef59faa82863454118a9d5ff7d8768ce0
sushil2817 <tony281723@gmail.com>
2024-04-23 13:59:20 +0530

    learning about react-redux & redux-toolkit

---
+++ react-redux/reduxtollkitodo/src/app/store.js
@@ -0,0 +1,11 @@
+// step 1: configure store
+
+
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice'
+
+
+export const store = configureStore({
+    reducer:todoReducer
+}) ;
+
