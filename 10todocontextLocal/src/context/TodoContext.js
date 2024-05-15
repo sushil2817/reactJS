1ae40169fb47861d9db81f7dbb4705d79e46e8d8
sushil2817 <tony281723@gmail.com>
2024-05-15 08:45:37 +0530

    trying to understand React Todo Project

--- 10todocontextLocal/src/context/TodoContext.js
+++ 10todocontextLocal/src/context/TodoContext.js
@@ -1,22 +1,22 @@
-import { createContext, useContext } from "react";
+import {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
-    todos:[
-        { 
-            id:1,
-            todo:"Todo msg",
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
             completed: false,
         }
     ],
-    addTodo:(todo)=>{},
-    updateTodo: (id,todo)=>{},
-    toggleComplete:(id)=>{}
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 })
 
 
-export const useTodo = () =>{
+export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-
-export const Todoprovider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider
